c7e275e4c9029df67f83ded20a903599a7804130
Christina Bui <uyen.buitie@gmail.com>
2023-06-04 22:13:57 -0500

    Homepage Mobile

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -7,46 +7,62 @@ import Case from './Homepage/Case';
 const licenseKey = '5OHLK-PXI0J-1K60I-BJ6SI-TOONN';
 
 class Homepage extends Component {
-    state = {};
-  
-  
-    render() {
-      const fullpageOptions = {
-          scrollingSpeed: 1000,
-          navigation: true,
-         
-        };
-        
-    
+  state = {};
+
+  render() {
+    const fullpageOptions = {
+      scrollingSpeed: 1000,
+      navigation: true,
+    };
+
+    // Check if the current device is a mobile device
+    const isMobileDevice = window.innerWidth <= 767;
+
+    if (isMobileDevice) {
+      // Render the sections without ReactFullpage on mobile devices
       return (
-          
-          <ReactFullpage
-          licenseKey={licenseKey}
-          {...fullpageOptions}
-          render={({ state, fullpageApi }) => {
-            return (
-              <ReactFullpage.Wrapper>
-            
-                <div className='section'>
-                  <Landing />
-                </div>
-                <div className='section'>
-                  <Tools />
-                </div>
-                <div className='section'>
-                  <Case />
-                </div>
-                <div className='section'>
-                  <Consulting />
-                </div>
-             
-                
-              </ReactFullpage.Wrapper>
-            );
-          }}
-        />
+        <div>
+          <div className='section'>
+            <Landing />
+          </div>
+          <div className='section'>
+            <Tools />
+          </div>
+          <div className='section'>
+            <Case />
+          </div>
+          <div className='section'>
+            <Consulting />
+          </div>
+        </div>
       );
     }
+
+    return (
+      <ReactFullpage
+        licenseKey={licenseKey}
+        {...fullpageOptions}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className='section'>
+                <Landing />
+              </div>
+              <div className='section'>
+                <Tools />
+              </div>
+              <div className='section'>
+                <Case />
+              </div>
+              <div className='section'>
+                <Consulting />
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    );
   }
-  
-  export default Homepage;
+}
+
+export default Homepage;
